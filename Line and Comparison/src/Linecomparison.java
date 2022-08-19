f837e2e72a71de84a7b227b82f448d8f086473e9
arc-015 <chavanavinash015@gmail.com>
2022-08-19 22:26:05 +0530

    Compare two lines are equal or not

--- Line and Comparison/src/Linecomparison.java
+++ Line and Comparison/src/Linecomparison.java
@@ -11,5 +11,15 @@ public class Linecomparison {
 				
 			double Distance=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)) ;
 			System.out.println("Distance between = "+"("+x1+","+y1+"),"+"("+x2+","+y2+") "+Distance);
-			}
-		}
+			
+			int lengthline1=2;
+			int lengthline2=2;
+			int d1= (int)(lengthline1);
+			int d2= (int)(lengthline2);
+			if (d1 ==d2)
+			     System.out.println("Line 1 and Line 2 have equal Length.");
+			else
+				 System.out.println("Line 1 and Line 2 have do not have equal length.");
+			
+	    }
+	 }
