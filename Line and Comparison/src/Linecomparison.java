54bccc3edb56a40ceca0fd93b4e05ffbeada8885
arc-015 <chavanavinash015@gmail.com>
2022-08-19 22:14:32 +0530

    calculating distance of Coordinate using cartesian system

--- Line and Comparison/src/Linecomparison.java
+++ Line and Comparison/src/Linecomparison.java
@@ -3,5 +3,13 @@ public class Linecomparison {
  
 	 public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Program");
-	}
-}
+	
+				int x1=1;
+				int y1=1;
+				int x2=5;
+				int y2=5;
+				
+			double Distance=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)) ;
+			System.out.println("Distance between = "+"("+x1+","+y1+"),"+"("+x2+","+y2+") "+Distance);
+			}
+		}
